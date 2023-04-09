a99398b3cda44eca9b5fafc14d1c7db373675f6a
kuzhanthaivelu <113583512+kuzhanthaivel@users.noreply.github.com>
2023-04-09 14:15:00 +0530

    Update ERC20Demo.sol

--- ERC20Demo.sol
+++ ERC20Demo.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
     uint256 public tokenPrice = 0 ether;
-
+    address owner; 
     constructor(uint256 initialSupply) ERC20("My Token", "MYT") {
         _mint(msg.sender, initialSupply);
     }
